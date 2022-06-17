f0cad4fd374e626a32552d5e92b91af38b422d02
alfredmjz <alfredmjz@gmail.com>
2022-06-17 17:19:10 -0400

    Implement and test favourite blog function

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -10,7 +10,23 @@ const totalLikes = (blogs) => {
 	return likes > 0 ? likes : 0;
 };
 
+const favoriteBlog = (blogs) => {
+	blogs.map((blog) => {
+		return (blog = {
+			title: blog.title,
+			author: blog.author,
+			likes: blog.likes,
+		});
+	});
+
+	const currentFav = blogs.reduce((previousBlog, currentBlog) => {
+		return previousBlog.likes > currentBlog.likes ? previousBlog : currentBlog;
+	}, blogs[0]);
+	return currentFav ? currentFav : {};
+};
+
 module.exports = {
 	dummy,
 	totalLikes,
+	favoriteBlog,
 };

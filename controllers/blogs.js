0cee891c6e5c6c2a580f255c38ced9075988fae8
alfredmjz <alfredmjz@gmail.com>
2022-06-27 16:47:22 -0400

    Implement and test delete/put requests

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -24,4 +24,29 @@ blogsRouter.post("/", async (request, response, next) => {
 	}
 });
 
+blogsRouter.delete("/:id", async (request, response, next) => {
+	try {
+		await Blog.deleteOne({ _id: request.params.id });
+		response.status(204).end();
+	} catch (err) {
+		next(err);
+	}
+});
+
+blogsRouter.put("/:id", async (request, response, next) => {
+	try {
+		const thisBlog = await Blog.findById(request.params.id);
+		const updatedBlog = {
+			title: request.body.title,
+			author: request.body.author,
+			url: request.body.url,
+			likes: request.body.likes || thisBlog.likes,
+		};
+		// await Blog.findByIdAndUpdate(request.params.id, updatedBlog, { new: true });
+		await Blog.updateOne(thisBlog, updatedBlog);
+		response.status(200).json(updatedBlog);
+	} catch (err) {
+		next(err);
+	}
+});
 module.exports = blogsRouter;

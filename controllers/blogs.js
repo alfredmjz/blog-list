50276303ae28f8808329298903e1b945ef914c74
alfredmjz <alfredmjz@gmail.com>
2022-06-29 00:45:02 -0400

    attach verification of token onto post adding

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
+const jwt = require("jsonwebtoken");
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
+
+const getTokenFrom = (request) => {
+	const authorization = request.get("authorization");
+	if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+		return authorization.substring(7);
+	}
+	return null;
+};
 
 blogsRouter.get("/", async (request, response, next) => {
 	try {
@@ -13,7 +22,12 @@ blogsRouter.get("/", async (request, response, next) => {
 
 blogsRouter.post("/", async (request, response, next) => {
 	try {
-		const user = await User.findById(request.body.userId);
+		const token = getTokenFrom(request);
+		const decodedToken = jwt.verify(token, process.env.SECRET);
+		if (!decodedToken.id) {
+			return response.status(401).json({ error: "token missing or invalid" });
+		}
+		const user = await User.findById(decodedToken.id);
 		const blog = new Blog({
 			user: user._id,
 			title: request.body.title,
